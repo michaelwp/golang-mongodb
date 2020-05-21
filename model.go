5f39ec07afdae32d1dce5d03baa6cb0173ad5d71
Michael WP <michaelwp@Michaels-MacBook-Air.local>
2020-05-21 19:21:28 +0700

    create insert and  view controller

---
+++ model.go
@@ -0,0 +1,15 @@
+package main
+
+type User struct {
+	_Id       int    `json:"_id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+type Response struct {
+	Status  int    `json:"status"`
+	Message string `json:"message"`
+	Data    []User
+}
--- router.go
+++ router.go
@@ -5,6 +5,8 @@ import "github.com/gorilla/mux"
 func router() *mux.Router {
 	router := mux.NewRouter()
 	router.HandleFunc("/", home).Methods("GET")
+	router.HandleFunc("/user", viewUser).Methods("GET")
+	router.HandleFunc("/user", addUser).Methods("POST")
 
 	return router
 }
